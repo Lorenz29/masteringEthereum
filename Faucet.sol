c8243716f5aa81217df258c3f3feb77ab2d7dae6
Lorenz29 <lorenzohardoy@hotmail.com>
2024-03-11 17:23:31 -0300

    Faucet creation

---
+++ Faucet.sol
@@ -0,0 +1,17 @@
+// My first contract is a faucet!
+contract Faucet {
+
+  //Give out ether to anyone who asks
+  function withdraw(uint withdraw_amount) public {
+
+    //Limit withdrawal amount
+    require(withdraw_amount <= 100000000000000000);
+
+    //send the amount to the address that requested it
+    msg.sender.transfer(withdraw_amount);
+  }
+
+  // Accept any incoming amount
+  function () public payable {}
+
+}
