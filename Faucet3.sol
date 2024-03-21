615c646ff93462800266b28fe944f7e8111d01e2
Lorenz29 <lorenzohardoy@hotmail.com>
2024-03-21 18:50:27 -0300

    contract inheritence

--- Faucet3.sol
+++ Faucet3.sol
@@ -3,19 +3,38 @@
 // Version of Solidity compiler this program was written for
 //https://github.com/ethereumbook/ethereumbook/blob/develop/07smart-contracts-solidity.asciidoc#data-types
 
-pragma solidity ^0.6.0;
+pragma solidity ^0.4.22;
+
+contract owned {
+    address owner;
+
+    //Contract constructor: set owner
+    constructor() {
+        owner = msg.sender;
+    }
+    //Access control modifier
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+}
+
+contract mortal is owned {
+    // Contract destructor
+    function destroy() public onlyOwner {
+        selfdestruct(owner);
+    }
+}
 
 // Our first contract is a faucet!
-contract Faucet {
-    // Accept any incoming amount
-    receive() external payable {}
-
+contract Faucet is mortal {
     // Give out ether to anyone who asks
     function withdraw(uint withdraw_amount) public {
         // Limit withdrawal amount
         require(withdraw_amount <= 0.1 ether);
-
         // Send the amount to the address that requested it
         msg.sender.transfer(withdraw_amount);
     }
+    // Accept any incoming amount
+    function () public payable{}
 }
